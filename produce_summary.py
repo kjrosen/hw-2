ffcf4caf601ea2cdf1465e2c660959add253c67c
Kaitlin Rosen <kayejrosen@gmail.com>
2022-03-16 19:22:29 -0700

    compressed function and calling

--- produce_summary.py
+++ produce_summary.py
@@ -1,30 +1,22 @@
 '''inputs a daily file and returns the delivery summary'''
-def generate_summary(the_file):
-    for line in the_file:
+def generate_summary(day,the_file):
+    '''reaches out into a daily file'''
+    print(f"Day ", day)
+    ## opens the file and makes it a giant string
+    delivery_log = open(the_file)
+
+    for line in delivery_log:
         line = line.rstrip()##removes spaces to the right
         words = line.split('|')##makes a list of each item in the listed
 
-        ##confirms each section into a solo variable. unnecessary
-        melon = words[0]
-        count = words[1]
-        amount = words[2]
+        ##confirms each section into a solo variable. unnecessary except for clarity
+        melon, count, amount = words
 
         print(f"Delivered {count} {melon}s for total of ${amount}")
+    
+    delivery_log.close()
 
-print("Day 1")
-the_file = open("um-deliveries-day-1.txt")
-##filled with lines of - Mellon|Count|Amount
-generate_summary(the_file)
-the_file.close()
-
-
-print("Day 2")
-the_file = open("um-deliveries-day-2.txt")
-generate_summary(the_file)
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-day-3.txt")
-generate_summary(the_file)
-the_file.close()
+##each file filled with lines of - Mellon|Count|Amount
+generate_summary(1,"um-deliveries-day-1.txt")
+generate_summary(2,"um-deliveries-day-1.txt")
+generate_summary(3,"um-deliveries-day-1.txt")
